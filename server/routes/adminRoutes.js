57d05c30d951c6de6e926fd8b886fc85a5565cbf
Ebisa Getachew <ebisagetachew1243@gmail.com>
2026-02-26 23:19:36 +0300

    fixed routes

--- server/routes/adminRoutes.js
+++ server/routes/adminRoutes.js
@@ -5,6 +5,7 @@ import {
   resetRag,
   getUsers,
   updateUserRole,
+  deleteUser,
   getAnalytics,
 } from '../controllers/adminController.js';
 import { protect, admin } from '../middlewares/authMiddleware.js';
@@ -20,6 +21,7 @@ router.get('/documents', getDocuments);
 router.post('/reset-rag', resetRag);
 router.get('/users', getUsers);
 router.put('/users/:id/role', updateUserRole);
+router.delete('/users/:id', deleteUser);
 router.get('/analytics', getAnalytics);
 
 export default router;
