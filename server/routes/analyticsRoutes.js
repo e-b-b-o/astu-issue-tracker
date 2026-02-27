dab8f2812dab2f8dc1afed3f1e79eea67b157a13
Ebisa Getachew <ebisagetachew1243@gmail.com>
2026-02-27 11:27:34 +0300

    Remove old project files and restructure repository

--- server/routes/analyticsRoutes.js
+++
@@ -1,9 +0,0 @@
-import express from 'express';
-import { getAnalytics } from '../controllers/analyticsController.js';
-import { protect } from '../middlewares/authMiddleware.js';
-
-const router = express.Router();
-
-router.get('/', protect, getAnalytics);
-
-export default router;
